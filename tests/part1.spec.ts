13d1f785c37594fbb2a8aa619192ba31cd95dc99
Jorazon <oskari.pahkala@gmail.com>
2023-04-11 14:27:07 +0300

    add tests for kummeli S03E05

--- tests/part1.spec.ts
+++ tests/part1.spec.ts
@@ -2,12 +2,14 @@ import { test, expect } from "@playwright/test";
 
 // Add consent cookie.
 test.beforeEach(async ({ page, context }) => {
-	context.addCookies([{
-		name: 'yleconsent',
-		value: "v1|",
-		domain: '.yle.fi',
-		path: '/',
-	}])
+	context.addCookies([
+		{
+			name: "yleconsent",
+			value: "v1|",
+			domain: ".yle.fi",
+			path: "/",
+		},
+	]);
 });
 
 /*
@@ -29,25 +31,35 @@ test.describe("Main page", () => {
 		// Wait until login/register iframe is loaded
 		await page.waitForLoadState("networkidle");
 
-		let login_form_locator = await page.locator('.tunnus-sdk__iframe');
-		let login_form_frame_locator = await login_form_locator.frameLocator(':scope');
+		let login_form_locator = await page.locator(".tunnus-sdk__iframe");
+		let login_form_frame_locator = await login_form_locator.frameLocator(":scope");
 
 		// Click register button
 		await login_form_frame_locator.locator("button.register-button").click();
 
 		await page.waitForLoadState("networkidle");
 
-		// Fill the form 
-		await login_form_frame_locator.getByRole("textbox", { name: "Sähköposti", exact: true }).fill("test@test,test"); // Intentional mistaken comma in email
-		await login_form_frame_locator.getByRole("textbox", { name: "Salasana", exact: true }).fill("test1234");
-		await login_form_frame_locator.getByRole("combobox", { name: "Kuukausi" }).selectOption({ index: Math.floor(Math.random() * 12) });
-		await login_form_frame_locator.getByRole("combobox", { name: "Vuosi" }).selectOption({ index: Math.floor(Math.random() * 110) });
+		// Fill the form
+		await login_form_frame_locator
+			.getByRole("textbox", { name: "Sähköposti", exact: true })
+			.fill("test@test,test"); // Intentional mistaken comma in email
+		await login_form_frame_locator
+			.getByRole("textbox", { name: "Salasana", exact: true })
+			.fill("test1234");
+		await login_form_frame_locator
+			.getByRole("combobox", { name: "Kuukausi" })
+			.selectOption({ index: Math.floor(Math.random() * 12) });
+		await login_form_frame_locator
+			.getByRole("combobox", { name: "Vuosi" })
+			.selectOption({ index: Math.floor(Math.random() * 110) });
 
 		// Submit
 		await login_form_frame_locator.getByRole("button", { name: "Luo Tunnus" }).click();
 
 		// Expect email in wrong format message to exist
-		expect(await login_form_frame_locator.getByText("Tarkista sähköpostiosoitteen muoto.").count()).toBeGreaterThan(0);
+		expect(
+			await login_form_frame_locator.getByText("Tarkista sähköpostiosoitteen muoto.").count(),
+		).toBeGreaterThan(0);
 	});
 });
 
@@ -59,18 +71,19 @@ test.describe("TV Guide", () => {
 		await page.getByRole("checkbox", { name: "Näytä menneet ohjelmat" }).click();
 
 		// find all scheduled program cards
-		let cards = await page.locator(".schedule-card")
+		let cards = await page
+			.locator(".schedule-card")
 			// Filter programs that begin at 10 pm
 			.filter({
 				has: page.locator('time[itemprop="startDate"]', {
 					hasText: "22.00",
-				})
+				}),
 			})
 			// Filter programs titled "Kymmenen uutiset"
 			.filter({
 				has: page.locator('span[itemprop="name"]', {
 					hasText: "Kymmenen uutiset",
-				})
+				}),
 			});
 
 		// One and only one should exist
@@ -82,24 +95,24 @@ test.describe("TV Guide", () => {
 
 		// Expected values
 		const channels = [
-			{ "label": "Yle TV1", "style": "yle-tv1_vtc.png" },
-			{ "label": "Yle TV2", "style": "yle-tv2_vtc.png" },
-			{ "label": "Yle Teema Fem", "style": "yle-teema-fem_vt.png" },
-			{ "label": "Yle Areena", "style": "yle-areena_vt.png" },
-			{ "label": "MTV3", "style": "MTV3_vtc.png" },
-			{ "label": "Nelonen", "style": "Nelonen_vtc.png" },
-			{ "label": "Sub", "style": "Sub_vtc.png" },
-			{ "label": "TV5", "style": "TV5_vt.png" },
-			{ "label": "Liv", "style": "Liv_vtc.png" },
-			{ "label": "JIM", "style": "JIM_vtc.png" },
-			{ "label": "Kutonen", "style": "Kutonen_vtc.png" },
-			{ "label": "TLC", "style": "TLC_vtc.png" },
-			{ "label": "STAR Channel", "style": "STAR%20Channel_vtc.png" },
-			{ "label": "Ava", "style": "Ava_vtc.png" },
-			{ "label": "Hero", "style": "Hero_vtc.png" },
-			{ "label": "Frii", "style": "Frii_vtc.png" },
-			{ "label": "National Geographic", "style": "National%20Geographic_vt.png" },
-			{ "label": "TV Finland", "style": "tv-finland_vt.png" },
+			{ label: "Yle TV1", style: "yle-tv1_vtc.png" },
+			{ label: "Yle TV2", style: "yle-tv2_vtc.png" },
+			{ label: "Yle Teema Fem", style: "yle-teema-fem_vt.png" },
+			{ label: "Yle Areena", style: "yle-areena_vt.png" },
+			{ label: "MTV3", style: "MTV3_vtc.png" },
+			{ label: "Nelonen", style: "Nelonen_vtc.png" },
+			{ label: "Sub", style: "Sub_vtc.png" },
+			{ label: "TV5", style: "TV5_vt.png" },
+			{ label: "Liv", style: "Liv_vtc.png" },
+			{ label: "JIM", style: "JIM_vtc.png" },
+			{ label: "Kutonen", style: "Kutonen_vtc.png" },
+			{ label: "TLC", style: "TLC_vtc.png" },
+			{ label: "STAR Channel", style: "STAR%20Channel_vtc.png" },
+			{ label: "Ava", style: "Ava_vtc.png" },
+			{ label: "Hero", style: "Hero_vtc.png" },
+			{ label: "Frii", style: "Frii_vtc.png" },
+			{ label: "National Geographic", style: "National%20Geographic_vt.png" },
+			{ label: "TV Finland", style: "tv-finland_vt.png" },
 		];
 
 		const headers = await page.locator(".channel-header__logo").all();
@@ -108,14 +121,16 @@ test.describe("TV Guide", () => {
 		expect(headers.length).toBe(channels.length);
 
 		// Map to attribute object
-		const matches = await Promise.all(headers.map(async (element, index) => {
-			let label = await element.getAttribute("aria-label");
-			let style = await element.getAttribute("style");
+		const matches = await Promise.all(
+			headers.map(async (element, index) => {
+				let label = await element.getAttribute("aria-label");
+				let style = await element.getAttribute("style");
 
-			//console.log(index, label, style);
+				//console.log(index, label, style);
 
-			return { "label": label, "style": style };
-		}));
+				return { label: label, style: style };
+			}),
+		);
 
 		// Compare labels
 		const labels_match = matches.every(({ label, style }, index) => {
@@ -137,6 +152,35 @@ test.describe("TV Guide", () => {
 
 		expect(labels_match).toBeTruthy();
 		expect(images_match).toBeTruthy();
+	});
+});
 
+test.describe("Kummeli", () => {
+	test("Find date of airing of S03E05", async ({ page, browserName }) => {
+		await page.goto("https://areena.yle.fi/1-3339547");
+		await page.getByRole("button", { name: "Kausi 3" }).click();
+		await page.waitForLoadState("networkidle");
+
+		const episodes = await page.locator(".CardPage_listItem__JRVg1");
+		console.log("Episode count: ", await episodes.count());
+
+		const ep5 = await episodes.nth(4);
+		await ep5.screenshot({ path: `img/${browserName}/kummeli airing.png` });
+
+		expect(await ep5.getByText("ti 8.3.2016").count()).toBe(1);
+	});
+
+	test("Find name of S03E05", async ({ page, browserName }) => {
+		await page.goto("https://areena.yle.fi/1-3339547");
+		await page.getByRole("button", { name: "Kausi 3" }).click();
+		await page.waitForLoadState("networkidle");
+
+		const episodes = await page.locator(".CardPage_listItem__JRVg1");
+		console.log("Episode count: ", await episodes.count());
+
+		const ep5 = await episodes.nth(4);
+		await ep5.screenshot({ path: `img/${browserName}/kummeli name.png` });
+
+		expect(await ep5.getByText("5. Kummeli ").count()).toBe(1);
 	});
 });
